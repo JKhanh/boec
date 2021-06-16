883634cff58996c1036cd166fb0c323074858be0
Nguyen Quoc Khanh <nguyenkhanhcv@gmail.com>
2021-06-16 22:34:58 +0700

    Remove cart item after place order

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
-from django.shortcuts import redirect, reverse
-from .models import *
-from cart.models import Cart
+
 from accounts.models import Profile
+from cart.models import Cart
+from .models import *
+
+
 # Create your views here.
 
 
@@ -16,12 +20,13 @@ def order_save(request):
         orderItem, created = OrderItem.objects.update_or_create(
             order=order, product=item.product, price=item.price, quantity=item.quantity)
         order.order_items.add(orderItem)
+        item.delete()
     order.address = request.POST['address']
     order.postal_code = request.POST['zip']
     order.country = request.POST['country']
     order.state = request.POST['state']
     order.save()
-    
+
     return redirect('../../payment/'+str(order.id)+'/process')
 
 
